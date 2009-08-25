cbd3a8d611f14f4e67f91e4f92174aca3f4017fc
voidy21 <y2.void@gmail.com>
2009-08-25 18:28:43 +0900

    vimヘルプ日本語化とか

---
+++ .vimperator/plugin/xpath_hint.js
@@ -0,0 +1,82 @@
+/*** BEGIN LICENSE BLOCK {{{
+    Copyright (c) 2009 hogelog<konbu.komuro@gmail.com>
+
+    distributable under the terms of an MIT-style license.
+    http://www.opensource.jp/licenses/mit-license.html
+}}}  END LICENSE BLOCK ***/
+// PLUGIN_INFO//{{{
+var PLUGIN_INFO =
+<VimperatorPlugin>
+    <name>{NAME}</name>
+    <description>add "get element's XPath" hint mode</description>
+    <author mail="konbu.komuro@gmail.com" homepage="http://d.hatena.ne.jp/hogelog/">hogelog</author>
+    <version>0.1.2</version>
+    <minVersion>2.0pre</minVersion>
+    <maxVersion>2.0pre</maxVersion>
+    <updateURL>http://svn.coderepos.org/share/lang/javascript/vimperator-plugins/trunk/xpath_hint.js</updateURL>
+    <license>MIT</license>
+    <detail><![CDATA[
+
+== MAPPING ==
+;x:
+    copy selected element's XPath
+
+]]></detail>
+</VimperatorPlugin>;
+//}}}
+
+(function(){
+const DEFAULT_MAP = "x";
+
+let xpathHintMap = liberator.globalVariables.xpath_hint_map || DEFAULT_MAP;
+
+let xh = plugins.xpath_hint = {
+    getElementXPath: function(elem)
+    {
+        if (elem.nodeType == 9) { // DOCUMENT_NODE = 9
+            return "";
+        }
+        if (elem.hasAttribute("id")) {
+            return 'id("'+elem.getAttribute("id")+'")';
+        }
+        let name = elem.tagName.toLowerCase();
+        let parent = elem.parentNode;
+        let path = arguments.callee(parent)+"/"+name;
+        let children = Array.filter(parent.childNodes, function(e) e.nodeName == elem.nodeName && e.nodeType == elem.nodeType);
+
+        if (children.length != 1 && children[0]!=elem) {
+            path += "["+(children.indexOf(elem)+1)+"]";
+        }
+        return path;
+    },
+    addMode: function(mode, prompt, action, tags)
+    {
+        hints.addMode(mode, prompt,
+                function(e) action(xh.getElementXPath(e)), tags);
+    },
+};
+xh.addMode(xpathHintMap, "copy xpath",
+    function(xpath) util.copyToClipboard(xpath, true),
+//    clickElement,
+    function() "//*");
+
+})();
+// 要素をクリックする
+function clickElement (xpath) {
+  var result;
+  try {
+     result = buffer.evaluateXPath(xpath.string);
+  } catch(e) {
+     liberator.echoerr('XPath : ' + e);
+  }
+  if (!result.snapshotLength){
+     liberator.echo('XPath : none');
+     return;
+  }
+  var elem = result.snapshotItem(0);
+  liberator.echo('click: ' + elem);
+  var e = content.document.createEvent('MouseEvents');
+  e.initMouseEvent('click', true, true, window, 1, 0, 0, 0, 0, false, false, false, false, 0, null);
+  elem.dispatchEvent( e );
+}
+// vim: set fdm=marker sw=4 ts=4 et:
