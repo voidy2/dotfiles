d85bbc1e275580e450ce572f1fd368c657b300d4
voidy21 <y2.void@gmail.com>
2009-09-20 02:49:11 +0900

    ラベル指定で未読記事を読めるようにした(:grlコマンド)

--- .vimperator/plugin/greader.js
+++ .vimperator/plugin/greader.js
@@ -5,7 +5,7 @@ var PLUGIN_INFO =
   <description>Open Google Reader starred items</description>
   <description lang="ja">Google Reader でスターを付けたページを開く</description>
   <author mail="y2.void@gmail.com" homepage="http://d.hatena.ne.jp/voidy21/">voidy21</author>
-  <version>0.1.1</version>
+  <version>0.2.0</version>
   <minVersion>2.2pre</minVersion>
   <maxVersion>2.2pre</maxVersion>
   <updateURL>http://github.com/voidy21/dotfiles/raw/master/.vimperator/plugin/greader.js</updateURL>
@@ -20,6 +20,7 @@ var PLUGIN_INFO =
 
    == Commands ==
     :[count]gr[eader]
+    :gr[label]
 
    == Global variables ==
     g:greaderOpenItemsCount:
@@ -70,17 +71,25 @@ var PLUGIN_INFO =
       <TAB>で補完候補の一覧にスターを付けた記事の一覧から選択することもできます。
       count を指定すると、その件数だけ開きます。
 
+    :grl[abel] :
+      このコマンドはラベル指定によって未読の記事をバッググラウンドのタブで開きます。
+      まずGoogleReaderのラベルを補完候補から<TAB>で選び、スペースキーで空白を入力します。
+      するとさらにラベルが付いた記事の一覧のURLの補完候補が出てくるので、<TAB>で選び
+      <Enter>で決定します。
+      また、-allitemオプションによって既読の記事も表示させることができます。
+      さらに、XUL/Migemo拡張を使用している場合、ラベルについてはMigemo補完が可能です。
+
     == グローバル変数 ==
     g:greaderOpenItemsCount:
       一度に開くスター付き記事の数
       default: 5
 
     g:greaderViewItemsCount:
-      補完候補の一覧に表示されるスター付き記事の数
+      補完候補の一覧に表示される記事の数
       default: 20
 
     g:greaderOpenBehavior:
-      スター付き記事を開くときの挙動、liberator.open()の第2引数として使用する値
+      記事を開くときの挙動、liberator.open()の第2引数として使用する値
       参考）http://wiki.livedoor.jp/shin_yan/d/liberator%282%2e0%29#content_34
       default: liberator.NEW_BACKGROUND_TAB
 
@@ -113,13 +122,28 @@ var PLUGIN_INFO =
       linkに該当するスターを一覧から削除する。
       ただし、最新のスターからg:greaderViewItemsCountで指定した数(デフォルトでは20)までしか辿らないので注意。
 
+    == ChangeLog ==
+      - 0.2.0
+      -- ラベル指定で記事を読めるようにした
+
     == TODO ==
-      - タグ指定で読めるようにする
-      - 未読か既読か決められるようにする
+      - 指定したURLのGoogleReaderのフィード登録を出来るようにする
   ]]></detail>
 </VimperatorPlugin>;
 //}}}
 let self = liberator.plugins.greader = (function() {
+  // FOR MIGEMO EXTENTION ////////////////////////////////////////////// {{{
+
+  try{
+    var XMigemoCore = Components
+      .classes['@piro.sakura.ne.jp/xmigemo/factory;1']
+      .getService(Components.interfaces.pIXMigemoFactory)
+      .getService("ja");
+  }catch(ex){
+    var XMigemoCore = undefined;
+  }
+
+ // }}}
   // COMMAND /////////////////////////////////////////////////////// {{{
   commands.addUserCommand(
     ["gr[eader]"],
@@ -127,7 +151,6 @@ let self = liberator.plugins.greader = (function() {
     function(args) {
       var stars = new Stars();
       var items = stars.items();
-
       if (!items || items.length == 0) {
         liberator.echo("starred item doesn't exists.");
         return;
@@ -153,7 +176,7 @@ let self = liberator.plugins.greader = (function() {
       count: true,
       completer: function(context) {
         context.format = {
-          anchored: true,
+          anchored: false,
           title: ["TITLE & URL","SOURCE"],
           keys: {
             text: "url",
@@ -162,14 +185,72 @@ let self = liberator.plugins.greader = (function() {
           },
           process: [templateTitleAndUrl,templateSourceTitle]
         };
-        context.completions = generateCandidates(false);
+        context.completions = generateCandidates(false,(new Stars()).items());
       },
       options: [
       ]
     },
     true
   );
-  // }}}
+  commands.addUserCommand(
+    ["grl[abel]"],
+    "view Google Reader select label feed",
+    function(args) {
+      var gapi = new GoogleApiController();
+      if (args.string == "") {
+        return;
+      } else {
+        if( args[0].indexOf("http://",0) != 0 )
+          return;
+        liberator.open(args[0], openBehavior());
+        setTimeout(function(e_id) { gapi.setReadId(e_id)}, 10 , gapi.getEntryIdByLabel(args[0],comp_tag));
+        return;
+      }
+    },
+    {
+      literal: 0,
+      count: false,
+      completer: function(context, args) {
+        let filter = context.filter;
+        let gapi = new GoogleApiController();
+        let labels = gapi.getLabels();
+        if ( filter.match("] ") == null ){
+          var match_result = filter.match(/((?:\[[^\]]*\])+)?\[?(.*)/); //[all, commited, now inputting]
+          var m = new RegExp(XMigemoCore ? "^(" + XMigemoCore.getRegExp(match_result[2]) + ")"
+                                         : "^" + match_result[2],'i');
+          var completionList = [];
+          labels.forEach(function(label){
+            if(m.test(label)){
+              completionList.push(["[" + label + "]","Google Reader Label"]);
+            }
+          });
+          context.title = ['Label','Description'];
+          context.completions = completionList;
+        } else {
+          comp_tag = context.filter.match(/\[(.*)\]/)[1];
+          context.filter = filter.replace("[" + comp_tag + "]","");
+          context.format = {
+            anchored: true,
+            title: ["TITLE & URL (Label : " + comp_tag + ")","SOURCE"],
+            keys: {
+              text: "url",
+              title:"title",
+              sourceTitle: "sourceTitle",
+            },
+            process: [templateTitleAndUrl,templateSourceTitle]
+          };
+          var isViewUnreadItem = args["-allitem"] ? false : true;
+          context.completions = generateCandidates(isViewUnreadItem,gapi.getFeed(comp_tag));
+        }
+      },
+      options: [
+        //[['-unread', '-u'], commands.OPTION_NOARG],
+        [['-allitem', '-a'], commands.OPTION_NOARG],
+      ]
+    },
+    true
+  );
+ // }}}
   // GLOBAL VARIABLES ////////////////////////////////////////////// {{{
   var gv = liberator.globalVariables;
 
@@ -181,6 +262,8 @@ let self = liberator.plugins.greader = (function() {
 
   function viewItemsCount()
     gv.greaderViewItemsCount || 20;
+
+  let comp_tag = null;
 
   // }}}
   // CLASS ///////////////////////////////////////////////////////// {{{
@@ -196,6 +279,8 @@ let self = liberator.plugins.greader = (function() {
     this.ATOM_STATE_READ = this.ATOM_PREFIXE_STATE_GOOGLE + 'read';
     this.ATOM_STATE_READING_LIST = this.ATOM_PREFIXE_STATE_GOOGLE + 'reading-list';
     this.ATOM_STATE_STARRED = this.ATOM_PREFIXE_STATE_GOOGLE + 'starred';
+    this.API_LIST_TAG = 'tag/list';
+    this.API_EDIT_TAG = 'edit-tag';
   }
   GoogleApiController.prototype = {
     token : function(){
@@ -249,6 +334,47 @@ let self = liberator.plugins.greader = (function() {
       request.get();
       return result;
     },
+
+    getLabels : function() {
+      var result = [];
+      var request = new libly.Request(
+        this.URI_PREFIXE_API + this.API_LIST_TAG,
+        null,
+        {
+          asynchronous: false,
+        }
+      );
+      request.addEventListener("onSuccess", function(data) {
+        var response = data.responseText;
+        var e4x = new XML(response);
+        var lists = e4x.list.object.string.(@name=="id");
+        for each (var e in lists ) {
+          var str = e.toString();
+          if ( str.indexOf("state/com.",0) < 0 )
+            result.push(str.slice(32,str.length));
+         }
+      });
+      request.addEventListener("onFailure", function(data) {
+        liberator.echoerr("Failure: Can't get token");
+      });
+      request.get();
+      return result;
+    },
+
+    edit_api : function(postArgs) {
+      var request = new libly.Request(
+        this.URI_PREFIXE_API + this.API_EDIT_TAG,
+        null,
+        {
+          postBody: toQuery(postArgs)
+        }
+      );
+      request.addEventListener("onFailure", function(data) {
+        liberator.echoerr("Cannot request post!");
+      });
+      request.post();
+    },
+
     getFeed : function(label) {
        var result = null;
        var feedUri = this.URI_PREFIXE_ATOM;
@@ -304,7 +430,25 @@ let self = liberator.plugins.greader = (function() {
        request.get();
        return result;
     },
-}
+
+    getEntryIdByLabel : function(url,label) {
+      var items = this.getFeed(label);
+      for each( var e in  items) {
+        if(e.link == url)
+          return e.id;
+      }
+      return null;
+    },
+
+    setReadId : function(entry_id) {
+      this.edit_api({
+        i : entry_id,
+        a : "user/" + getGreaderUserId() + "/state/com.google/read",
+        T : this.token(),
+        async : "false",
+      });
+    },
+  }
 
   function Stars() {
     this.cache_items = null;
@@ -345,26 +489,14 @@ let self = liberator.plugins.greader = (function() {
     removeId : function(entry_id, isRemove) {
       if( !this.isRemoveEnable && isRemove == undefined )
         return;
-      var request = new libly.Request(
-        "http://www.google.com/reader/api/0/" + "edit-tag" + "?client=contact:myname-at-gmail",
-        null,
-        {
-          postBody: toQuery({
-            i : entry_id,
-            r : "user/" + getGreaderUserId() + "/state/com.google/starred",
-            T : this.gapi.token(),
-            async : "false",
-          })
-        }
-      );
-      request.addEventListener("onSuccess", function(data) {
-        liberator.log("Removed star from '" + entry_id + "' was succeeded.");
+      this.gapi.edit_api({
+        i : entry_id,
+        r : "user/" + getGreaderUserId() + "/state/com.google/starred",
+        T : this.gapi.token(),
+        async : "false",
       });
-      request.addEventListener("onFailure", function(data) {
-        liberator.echoerr("Cannot remove star");
-      });
-      request.post();
     },
+
     getEntryId : function(url) {
       var items = this.items();
       for each( var e in  items) {
@@ -400,13 +532,12 @@ let self = liberator.plugins.greader = (function() {
   * Get candidates list
   * @return [{"url": "(url)", "title": "hogehoge", "sourceTitle": "hogefuga"}, ... ]
   */
-  function generateCandidates(isUnreadFilter) {
+  function generateCandidates(isUnreadFilter,items) {
     let allEntries = [];
-    let stars = new Stars();
-    let items = stars.items();
+    let _items = items;
     if( isUnreadFilter )
-        items = filterUnReadEntry(items);
-    for each (var e in items){
+      _items = filterUnReadEntry(items);
+    for each (var e in _items){
       var entries = {
         "url" : e.link,
         "title" : e.title,
@@ -420,8 +551,9 @@ let self = liberator.plugins.greader = (function() {
   function filterUnReadEntry(items){
     var result = [];
     for each(item in items){
-      if( item.category.indexOf("read\"",0) > 0)
+      if( item.category.indexOf('read\"',0) < 0){
         result.push(item);
+      }
     }
     return result;
   }
