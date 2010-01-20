b742c11b579d21e2fccaaace93eac759dbf127e9
voidy21 <y2.void@gmail.com>
2010-01-20 09:40:57 +0900

    anekosさんの改良版

--- .vimperator/plugin/opener.js
+++ .vimperator/plugin/opener.js
@@ -1,23 +1,34 @@
 (function () {
-    function opener(where) {
-        return function (args) {
-            let index = 0;
-            let url = util.stringToURLArray(args[0]).toString();
-            for each ( [,tab] in tabs.browsers ) {
-                if(url == tab.currentURI.spec){
-                    tabs.select(index);
-                    return;
-                }
-                ++index;
+    let U = liberator.plugins.libly.$U;
+
+    function jump (url) {
+        let index = 0;
+        let url = util.stringToURLArray(url).toString();
+        for each ( [,tab] in tabs.browsers ) {
+            if(url == tab.currentURI.spec){
+                tabs.select(index);
+                return true;
             }
-            liberator.open(url,where);
-        };
+            ++index;
+        }
+        return false;
     }
- 
-    let open = commands.get("open");
-    //let edit = commands.get("edit");
-    let tabopen = commands.get("tabopen");
-    open.action = opener(liberator.CURRENT_TAB);
-    //edit.action = opener(liberator.CURRENT_TAB);
-    tabopen.action = opener(liberator.NEW_TAB);
+
+    "open tabopen edit".split(/\s/).forEach(
+      function (name) {
+        let command = commands.get(name);
+        if (!command)
+          return;
+        U.around(
+          command,
+          "action",
+          function (next, args) {
+            let url = args[0].string;
+            if (!(url && jump(url)))
+              return next();
+          },
+          true
+        );
+      }
+    );
 })();
